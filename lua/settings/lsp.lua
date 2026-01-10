8f2e52ebfb03ba7800084ec98003ef754cc5ae15
joe <maloney.jg2@gmail.com>
2026-01-10 14:18:42 -0500

    black formatter autocommand for python

--- lua/settings/lsp.lua
+++ lua/settings/lsp.lua
@@ -132,3 +132,12 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     end,
     group = autocmd_group,
 })
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    pattern = { "*.py" },
+    desc = "Format python on save",
+    callback = function()
+        local fileName = vim.api.nvim_buf_get_name(0)
+        vim.cmd(":silent !black " .. fileName)
+    end,
+    group = autocmd_group,
+})
